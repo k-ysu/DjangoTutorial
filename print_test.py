12f86f7fb7a7eea52c93f0b1375f42db973a0305
yasushi <yaushikomori@yaushis-mbp.attlocal.net>
2018-04-01 13:12:02 -0700

    add

--- print_test.py
+++ print_test.py
@@ -1,6 +1,19 @@
 #!/usr/bin/env python
 
-age=27
-name="matt"
+age=12
+name="Matt"
 
-print (" Hello my name is {}. I am {} years old".format(name,age))
+#print (" Hello my name is {}. I am {} years old".format(name,age))
+# cfsd;fasdkfjas;fksdf;kdsaf;ldskfl;k;
+
+"""fdasfsadffsafadfasfdsafsadfa
+fdasfsadffsafadfasfdsafsadfa
+fdasfsadffsafadfasfdsafsadfafds
+fdasfsadffsafadfasfdsafsadfa"""
+
+if name == "Matt":
+    print("You are older than 18")
+    print("This is another line")
+    #fasdfsadfsdaf
+else:
+    print("You are younger than 18")
